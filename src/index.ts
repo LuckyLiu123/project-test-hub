10c667395119c49fec5f540baa4fe6fbf0557f8f
liukai <18229886430@163.com>
2023-05-07 16:21:39 +0800

    ci: add eslint and commitlint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-const calc = (a: number, b: number): number => {
-  return a - b
+const calc = (a: number, b: number): void => {
+  console.log(a - b);
 }
 
-console.log(calc(1024, 28))
+// console.log(calc(1024, 28))
+calc(1024, 28)
